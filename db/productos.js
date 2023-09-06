421054b50d43f2d52092a3f198ba4e9d032eb5f0
Nahuel Demarco <marco.denaju@gmail.com>
2023-09-06 16:54:29 -0300

    se corrigieron textos

--- db/productos.js
+++ db/productos.js
@@ -1,55 +1,55 @@
 export const productos = [
     { 
       id: 1, 
-      nombre: "sticker simple", 
+      nombre: "STICKER SIMPLE", 
       precio: 200, 
       imagen: "https://mipegatina.club/wp-content/uploads/2022/12/Blanco_5.png",
       categoria: "Stickers base"
     },
     { 
       id: 2, 
-      nombre: "sticker transparente", 
+      nombre: "STICKER TRANSPARENTE", 
       precio: 300, 
       imagen: "https://mipegatina.club/wp-content/uploads/2022/12/Transparente_3.png",
       categoria: "Stickers especiales"
     },
     { 
       id: 3, 
-      nombre: "sticker holo", 
+      nombre: "STICKER HOLO", 
       precio: 400, 
       imagen: "https://mipegatina.club/wp-content/uploads/2022/11/HoloTrama_2.png",
       categoria: "Stickers holo"
     },
     { id: 4,
-      nombre: "sticker glitter", 
+      nombre: "STICKER GLITTER", 
       precio: 500, 
       imagen: "https://mipegatina.club/wp-content/uploads/2022/11/HoloGlitter_3.png",
       categoria: "Stickers holo" 
     },
     { 
       id: 5, 
-      nombre: "sticker base laca", 
+      nombre: "STICKER BASE LACA", 
       precio: 200, 
       imagen: "https://mipegatina.club/wp-content/uploads/2022/12/Blanco-Con-Laca_1.png",
       categoria: "Stickers especiales"
     },
     { 
       id: 6, 
-      nombre: "sticker gold", 
+      nombre: "STICKER GOLD", 
       precio: 400, 
       img: "https://mipegatina.club/wp-content/uploads/2022/11/HoloGold_4.png",
       categoria: "Stickers premium"
     },
     { 
       id: 7, 
-      nombre: "sticker frosted", 
+      nombre: "STICKER FROSTED", 
       precio: 200, 
       imagen: "https://mipegatina.club/wp-content/uploads/2022/11/FrostedBlanco_3.png",
       categoria: "Stickers premium"
     },
     { 
       id: 8, 
-      nombre: "sticker glow", 
+      nombre: "STICKER GLOW", 
       precio: 200, 
       imagen: "https://mipegatina.club/wp-content/uploads/2022/12/Glow_1.png",
       categoria: "Stickers premium"
